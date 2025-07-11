34a275363dc5400be4c7f41dd2892e1dd2a6ee11
ReshmaR29 <reshmareddy2211@gmail.com>
2025-07-11 14:42:02 +0530

    Chore: add cart application to myshop project

--- myshop/settings.py
+++ myshop/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
-]
+    'cart.apps.CartConfig',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -126,3 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
+
+
