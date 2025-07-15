1e9190242cd87dc3ecab4b944165dd84a8ccfb84
ReshmaR29 <reshmareddy2211@gmail.com>
2025-07-15 17:54:04 +0530

    add translations to url patterns

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,11 @@
+from django.utils.translation import gettext_lazy as _
 from django.urls import path
 from . import views
 
 app_name = 'orders'
 
 urlpatterns = [
+    path(_('create/'), views.order_create, name='order_create'),
     path('order/create/', views.order_create, name='order_create'),
     path('admin/order/<int:order_id>/', views.admin_order_detail,
      name='admin_order_detail'),
